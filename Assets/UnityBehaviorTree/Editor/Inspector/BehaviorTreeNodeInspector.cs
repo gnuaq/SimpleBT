0a5a3e7fc6676041b08ae971d71b94ac870c8950
minquang <91359738+minquang@users.noreply.github.com>
2024-04-17 04:16:07 +0700

    unselect node when play mode change

--- Assets/UnityBehaviorTree/Editor/Inspector/BehaviorTreeNodeInspector.cs
+++ Assets/UnityBehaviorTree/Editor/Inspector/BehaviorTreeNodeInspector.cs
@@ -1,48 +1,22 @@
-﻿// using UnityBehaviorTree.Core;
-// using UnityEditor;
-// using UnityEngine;
-//
-// namespace UnityBehaviorTree.Editor.Inspector
-// {
-//     [CustomEditor(typeof(BTNode), true)]
-//     public class BehaviorTreeNodeInspector : UnityEditor.Editor
-//     {
-//         private readonly GUIContent _descriptionHeader = new GUIContent("Node Description");
-//         
-//         private SerializedProperty _nodeTitle;
-//         private SerializedProperty _nodeComment;
-//         
-//         protected virtual void OnEnable()
-//         {
-//             _nodeTitle = serializedObject.FindProperty("_nodeViewData.Title");
-//             _nodeComment = serializedObject.FindProperty("_nodeViewData.Comment");
-//         }
-//         
-//         public override void OnInspectorGUI()
-//         {
-//             serializedObject.Update();
-//             DrawDefaultInspector();
-//             DrawNodeInspector();
-//             DrawNodeDescription();
-//             
-//             // If the behaviour was edited, update the tree editor and repaint.
-//             if (GUI.changed)
-//             {
-//                 serializedObject.ApplyModifiedProperties();
-//             }
-//         }
-//
-//         private void DrawNodeInspector()
-//         {
-//             
-//         }
-//
-//         private void DrawNodeDescription()
-//         {
-//             EditorGUILayout.Space();
-//             EditorGUILayout.LabelField(_descriptionHeader, EditorStyles.boldLabel);
-//             EditorGUILayout.PropertyField(_nodeTitle);
-//             EditorGUILayout.PropertyField(_nodeComment);
-//         }
-//     }
-// }
+﻿using UnityBehaviorTree.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityBehaviorTree.Editor.Inspector
+{
+    [CustomEditor(typeof(BTNode), true)]
+    public class BehaviorTreeNodeInspector : UnityEditor.Editor
+    {
+        protected virtual void OnEnable()
+        {
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private void OnPlayModeStateChanged(PlayModeStateChange obj)
+        {
+            Selection.activeObject = null;
+        }
+
+    }
+}
